f9b7b7133aae9db58363f7caf0a23608ec9c4233
harish hasti <harishhasti95@gmail.com>
2020-09-25 07:15:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var div = document.createElement('div');
+
+var button = document.createElement('button');
+var play = document.createElement('a');
+play.setAttribute('href', 'game.html');
+
+button.appendChild(play);
+
+div.appendChild('button');
+
+
+document.body.appendChild(div);
